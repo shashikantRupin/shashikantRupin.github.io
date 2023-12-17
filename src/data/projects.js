976baf8e48d1a9029fbe5ec39675f2b051aeefce
Shashikant Rupin <shashikantrupin123@gmail.com>
2023-12-17 11:50:11 +0530

    update

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+
+
+export default function getProjects() {
+    return [
+        {
+
+            id: 1,
+            image: 'jioMart1.png',
+            projectTitle: "JioMart",
+            projectInfo: "Welcome to Jio-Mart, your ultimate shopping app for everyday essentials! Discover a diverse range of high-quality groceries and beverages, providing you with convenience and quality at your fingertips. Elevate your shopping experience with us as we bring sustainable and top-notch products to your doorstep, making your home a hub of culinary delight and comfort.",
+            techUsed: ["React", "Redux", "CSS", "Firebase"],
+            projectLinkHosted: "https://jiomart-com-rct-201-shashikantrupin.vercel.app/",
+            projectLinkGithub: "https://github.com/shashikantRupin/Jio-Mart",
+        },
+        {
+
+            id: 1,
+            image: 'jioMart1.png',
+            projectTitle: "JioMart",
+            projectInfo: "Welcome to Jio-Mart, your ultimate shopping app for everyday essentials! Discover a diverse range of high-quality groceries and beverages, providing you with convenience and quality at your fingertips. Elevate your shopping experience with us as we bring sustainable and top-notch products to your doorstep, making your home a hub of culinary delight and comfort.",
+            techUsed: ["React", "Redux", "CSS", "Firebase"],
+            projectLinkHosted: "https://jiomart-com-rct-201-shashikantrupin.vercel.app/",
+            projectLinkGithub: "https://github.com/shashikantRupin/Jio-Mart",
+        },
+        {
+            id: 2,
+            projectTitle: "Mine Wine",
+            image: "myWine.png",
+            projectInfo: "It's a clone of Mine-Wine app created using HTML, CSS, Javascript",
+            techUsed: ["HTML", "CSS", "Javascript"],
+            projectLinkHosted: "https://655e1490fd1a4138593e6c56--capable-eclair-1ae3b7.netlify.app/",
+            projectLinkGithub: "https://github.com/shashikantRupin/Wine-project",
+        },
+        {
+            id: 3,
+            projectTitle: "My portfolio",
+            image: 'portfolio_img.png',
+            projectInfo: "My portfolio is a testament to my skills, passion, and dedication. Within these pages, you'll find a collection of projects that I've carefully crafted, each representing a unique blend of creativity and expertise.",
+            techUsed: ["React", "HTML", "CSS"],
+            projectLinkHosted: "https://github.com/shashikantRupin",
+            projectLinkGithub: "https://github.com/shashikantRupin/shashikantRupin.github.io",
+        }
+    ];
+}
